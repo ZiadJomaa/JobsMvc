c7984edf5ef15ea976fb8b80cd02953514096f56
HagarMahmoud12 <hagarmahmoudkh@gmail.com>
2026-09-13 14:40:50 +0300

    merge ziad branch

---
+++ ViewModel/CustomRegisterViewModel.cs
@@ -0,0 +1,11 @@
+﻿using JobsMvc.Models.Enums;
+
+namespace JobsMvc.ViewModel
+{
+    public class CustomRegisterViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public UserType SelectedUserType { get; set; }
+    }
+}
